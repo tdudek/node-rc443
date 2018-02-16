bf33ab2faf46e3995c9c397b83e3d05e284f1851
Thomas Dudek <mail.dudek@gmail.com>
2018-02-17 00:01:57 +0100

    Add device API

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const RcSwitch = require('node-rcswitch2')
 const Hapi = require('hapi')
 const HapiBoomDecorators = require('hapi-boom-decorators')
+const deviceList = require('./devices.json')
 
 const send = code => {
   return new Promise((resolve, reject) => {
@@ -16,9 +17,77 @@ const send = code => {
   })
 }
 
+class Devices {
+  constructor(devices) {
+    this.devices = devices || []
+  }
+
+  register(device) {
+    this._validate(device)
+
+    if (this.contains(device.name)) {
+      throw Error(`device ${device.name} already exists`)
+    }
+
+    device.type = 'switch-device'
+
+    this.devices.push(device)
+    return device
+  }
+
+  list() {
+    return this.devices
+  }
+
+  get(name) {
+    const device = this.devices.find(d => d.name == name)
+    if (typeof device === 'undefined') {
+      return null
+    }
+    return new SwitchDevice(device)
+  }
+
+  contains(name) {
+    return this.get(name) !== null
+  }
+
+  _validate(device) {
+    if (typeof device.name === 'undefined') {
+      throw Error('device.name must not be empty')
+    }
+    if (device.codes == null || typeof device.codes !== 'object') {
+      throw Error('device.name must not be empty')
+    }
+    if (typeof device.codes.on !== 'number') {
+      throw Error('device.codes.on must be set and must be a number')
+    }
+    if (typeof device.codes.off !== 'number') {
+      throw Error('device.codes.on must be set and must be a number')
+    }
+    return device
+  }
+}
+
+class SwitchDevice {
+  constructor(device) {
+    this.device = device
+  }
+
+  on() {
+    return send(this.device.codes.on)
+  }
+
+  off() {
+    return send(this.device.codes.off)
+  }
+  
+}
+
+const devices = new Devices(deviceList)
+
 const server = new Hapi.Server({
   port: 3000,
-  host: 'localhost',
+  host: '0.0.0.0',
 })
 
 server.route({
@@ -33,8 +102,8 @@ server.route({
     return send(code)
       .then(() => {
         return {
-          'status': 'success',
-          'message': `code ${code} successfully sent`
+          code,
+          message: `code ${code} successfully sent`,
         }
       })
       .catch(err => {
@@ -42,6 +111,60 @@ server.route({
         console.error(message, err)
         return h.badImplementation(message)
       })
+  }
+})
+
+server.route({
+  method: 'GET',
+  path: '/api/devices',
+  handler: (request, h) => devices.list()
+})
+
+server.route({
+  method: 'POST',
+  path: '/api/devices',
+  handler: (request, h) => {
+    try {
+      const device = devices.register(request.payload)
+      return device
+    } catch (err) {
+      return h.badRequest(err)
+    }
+  }
+})
+
+server.route({
+  method: 'PATCH',
+  path: '/api/devices/{name}',
+  handler: (request, h) => {
+    const device = devices.get(request.params.name)
+    if (device === null) {
+      return h.notFound()
+    }
+
+    const command = request.payload.command
+    if (command === 'on') {
+      return device.on()
+        .then(() => {
+          return {
+            command,
+            message: 'command executed',
+          }
+        })
+        .catch(() => h.badImplementation())
+    }
+    if (command === 'off') {
+      return device.off()
+        .then(() => {
+          return {
+            command,
+            message: 'command executed',
+          }
+        })
+        .catch(() => h.badImplementation())
+    }
+
+    return h.badRequest()
   }
 })
 
